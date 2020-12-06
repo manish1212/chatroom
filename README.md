7b42594e6d179c2145a261520c283e6cfa057232
Manish <mp121995@gmail.com>
2020-12-05 16:44:18 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # chatroom
 Real time chat between clients (socket programming)
+
+
+# Tkinter and sockets are used (Python standard library)
+# nothing to install
+
+# A socket is an IP address + port number pair
+
---
+++ server.py
@@ -0,0 +1,17 @@
+#!user/bin/evn python3
+
+import threading
+import socket
+import argparse
+import os
+
+class Server(threading.Thread):
+
+    def __init__(self, host, port):
+        super().__init__()
+        self.connections = []
+        self.host = host
+        self.port = port
+    
+    def run(self):
+        pass
